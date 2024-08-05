f4446d730cb94ea58d95d3fd8d59ed91e2c5ddfb
JEEVAVASANTHAN-1045 <jiivavasanthan@gmail.com>
2024-08-05 08:37:44 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,44 +1,63 @@
 #include <gtest/gtest.h>
-#include "StringCalculator.h"
+#include "StringCalculator.h"  // Ensure this is the correct path to your header file
 
-TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
+TEST(StringCalculatorSumStringTests, ExpectZeroForEmptyInput) {
     int expectedresult = 0;
-    const char* input = "Hello, world!";
-    int result = add(input);
+    const char* input = "";
+    int result = sumString(input);
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
+TEST(StringCalculatorSumStringTests, ExpectZeroForSingleZero) {
     int expectedresult = 0;
     const char* input = "0";
-    int result = add(input);
+    int result = sumString(input);
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
+TEST(StringCalculatorSumStringTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
-    const char*  input = "1,2";
-    int result = add(input);
+    const char* input = "1,2";
+    int result = sumString(input);
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
+TEST(StringCalculatorSumStringTests, ExpectSumWithNewlineDelimiter) {
     int expectedresult = 6;
-    const char*  input = "1\n2,3";
-    int result =add(input);
+    const char* input = "1\n2,3";
+    int result = sumString(input);
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
+TEST(StringCalculatorSumStringTests, IgnoreNumbersGreaterThan1000) {
     int expectedresult = 1;
-    const char*  input = "1,1001";
-    int result =add(input);
+    const char* input = "1,1001";
+    int result = sumString(input);
     ASSERT_EQ(result, expectedresult);
 }
 
-TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
+TEST(StringCalculatorSumStringTests, ExpectSumWithCustomDelimiter) {
     int expectedresult = 3;
-    const char*  input = "//;\n1;2";
-    int result = add(input);
+    const char* input = "//;\n1;2";
+    int result = sumString(input);
     ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorSumStringTests, ExpectSumWithCustomDelimiterAtStart) {
+    int expectedresult = 2;
+    const char* input = "\n2";
+    int result = sumString(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(StringCalculatorSumStringTests, ExpectSumWithDelimiterofAnyLength) {
+    int expectedresult = 15;
+    const char* input = "//[*]\n12***3";
+    int result = sumString(input);
+    ASSERT_EQ(result, expectedresult);
+}
+
+TEST(ExceptionHandlingTest, NegativeNumberException) {
+    const char* input = "-1,2,3";
+    ASSERT_THROW(sumString(input), std::runtime_error);
 }
